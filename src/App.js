2c1a1fd7f7ebf87e6aab41c7da1caf656d40cca4
AlexisAlegria <alexisalegriav@gmail.com>
2022-01-14 15:08:57 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Testimonials from "./components/testimonials/Testimonials";
 import Topbar from "./components/topbar/Topbar";
 import Works from "./components/works/Works";
+import "./app.scss";
+import { useState } from "react";
 
 function App() {
+
+  const [menuOpen, setMenuOpen] = useState(true)
   return (
     <div className="app">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
-        <h1>Hello world</h1>
         <Intro />
         <Portfolio />
         <Works />
--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,9 +1,31 @@
 import "./topbar.scss"
+import {Person,Mail} from "@material-ui/icons"
 
-export default function Topbar() {
+export default function Topbar({menuOpen,setMenuOpen}) {
     return (
-        <div className="topbar">
-            
+        <div className={"topbar " + (menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">Alexis Alegría</a>
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                            <span>+56 9 5758 7447</span>
+
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                            <span>alexisalegriav@gmail.com</span>
+
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger">
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
