fe258754d84221319ba7c84c1e26416ca593dcc8
AlexisAlegria <alexisalegriav@gmail.com>
2022-01-17 14:18:18 -0300

    first commit

--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -1,9 +1,37 @@
-import "./works.scss"
+import "./works.scss";
 
 export default function Works() {
-    return (
-        <div className="works" id="works">
+
+    const data = [
+        {
             
+        }
+
+    ]
+
+  return (
+    <div className="works" id="works">
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src="./assets/cell-phone.png" alt="cell-phone"/>
+                </div>
+                <h1>Title</h1>
+                <p>Lorem ipsum dolor</p>
+                <span>Projects</span>
+              </div>
+            </div>
+            <div className="right">
+              <img src="./assets/three-little-dogs.jpg" alt="dogs"/>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+      <img src="./assets/arrow.png" className="arrow left" alt="arrow" />
+      <img src="./assets/arrow.png" className="arrow right" alt="arrow" />
+    </div>
+  );
 }
---
+++ src/data.js
@@ -0,0 +1,139 @@
+export const featuredPortfolio = [
+  {
+    id: 1,
+    title: "Social Media App",
+    img: "./assets/computer.png",
+  },
+  {
+    id: 2,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+  {
+    id: 3,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+  {
+    id: 4,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+  {
+    id: 5,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+];
+
+export const webPortfolio = [
+  {
+    id: 1,
+    title: "Social Media App",
+    img: "./assets/code.png",
+  },
+  {
+    id: 2,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+  {
+    id: 3,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+  {
+    id: 4,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+  {
+    id: 5,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+];
+
+export const mobilePortfolio = [
+  {
+    id: 1,
+    title: "Social Media App",
+    img: "./assets/programing.png",
+  },
+  {
+    id: 2,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+  {
+    id: 3,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+  {
+    id: 4,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+  {
+    id: 5,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+];
+
+export const designPortfolio = [
+  {
+    id: 1,
+    title: "Social Media App",
+    img: "./assets/meeting.png",
+  },
+  {
+    id: 2,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+  {
+    id: 3,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+  {
+    id: 4,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+  {
+    id: 5,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+];
+
+export const contentPortfolio = [
+  {
+    id: 1,
+    title: "Social Media App",
+    img: "./assets/earth.png",
+  },
+  {
+    id: 2,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+  {
+    id: 3,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+  {
+    id: 4,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+  {
+    id: 5,
+    title: "Social Media App",
+    img: "./assets/bank-itau.png",
+  },
+];
