79a4d7679481ef65f1306cfba801ec5428d770d6
AlexisAlegria <alexisalegriav@gmail.com>
2022-01-17 21:16:53 -0300

    first commit

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,9 +1,27 @@
-import "./contact.scss"
+import "./contact.scss";
+import {useState} from "react";
 
 export default function Contact() {
+    const [message, setMessage] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+        setMessage(true);
+    }
     return (
         <div className="contact" id="contact">
-            
+            <div className="left">
+                <img src="./assets/paper-plane.png" alt=""/>
+            </div>
+            <div className="right">
+                <h1>Contact me</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Email"/>
+                    <textarea placeholder="Message"></textarea>
+                    <button type="submit">Send</button>
+                    {message && <span>Thanks, I will reply as soon as I can</span>}
+                </form>
+            </div>
         </div>
     )
 }
--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "./works.scss";
 
 export default function Works() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
   const data = [
     {
       id: "1",
@@ -25,9 +28,15 @@ export default function Works() {
     },
   ];
 
+  const handleClick = (way) => {
+      way === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide-1 : 2):
+      setCurrentSlide(currentSlide<data.length -1? currentSlide + 1 : 0);
+  }
+
+
   return (
     <div className="works" id="works">
-      <div className="slider">
+      <div className="slider" style={{ transform: `translateX(-${currentSlide *100}vw)` }}>
         {data.map((d) => (
           <div className="container">
             <div className="item">
@@ -47,9 +56,9 @@ export default function Works() {
             </div>
           </div>
         ))}
-        <img src="./assets/arrow.png" className="arrow left" alt="arrow" />
-        <img src="./assets/arrow.png" className="arrow right" alt="arrow" />
       </div>
+        <img src="./assets/arrow.png" className="arrow left" alt="arrow" onClick={() => handleClick("left")}/>
+        <img src="./assets/arrow.png" className="arrow right" alt="arrow" onClick={() => handleClick()}/>
     </div>
   );
 }
