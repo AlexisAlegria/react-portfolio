01536d713672188bc50c635db72c885d4e80d35a
AlexisAlegria <alexisalegriav@gmail.com>
2022-01-20 18:15:25 -0300

    first commit

--- src/data.js
+++ src/data.js
@@ -1,29 +1,28 @@
 export const featuredPortfolio = [
   {
     id: 1,
-    title: "Social Media App",
+    title: "Final Bootcamp Project",
     img: "./assets/computer.png",
+    url: "https://github.com/AlexisAlegria/4G-final-project-backend"
   },
   {
     id: 2,
-    title: "Social Media App",
+    title: "Available Box",
     img: "./assets/bank-itau.png",
+    url: "https://google.com"
   },
   {
     id: 3,
-    title: "Social Media App",
+    title: "Available Box",
     img: "./assets/bank-itau.png",
+    url: "https://google.com"
   },
   {
     id: 4,
-    title: "Social Media App",
+    title: "Available Box",
     img: "./assets/bank-itau.png",
-  },
-  {
-    id: 5,
-    title: "Social Media App",
-    img: "./assets/bank-itau.png",
-  },
+    url: "https://google.com"
+  }
 ];
 
 export const webPortfolio = [
@@ -31,26 +30,25 @@ export const webPortfolio = [
     id: 1,
     title: "Social Media App",
     img: "./assets/code.png",
+    url: "https://google.com"
   },
   {
     id: 2,
     title: "Social Media App",
     img: "./assets/bank-itau.png",
+    url: "https://google.com"
   },
   {
     id: 3,
     title: "Social Media App",
     img: "./assets/bank-itau.png",
+    url: "https://google.com"
   },
   {
     id: 4,
     title: "Social Media App",
     img: "./assets/bank-itau.png",
-  },
-  {
-    id: 5,
-    title: "Social Media App",
-    img: "./assets/bank-itau.png",
+    url: "https://google.com"
   },
 ];
 
@@ -59,26 +57,25 @@ export const mobilePortfolio = [
     id: 1,
     title: "Social Media App",
     img: "./assets/programing.png",
+    url: "https://google.com"
   },
   {
     id: 2,
     title: "Social Media App",
     img: "./assets/bank-itau.png",
+    url: "https://google.com"
   },
   {
     id: 3,
     title: "Social Media App",
     img: "./assets/bank-itau.png",
+    url: "https://google.com"
   },
   {
     id: 4,
     title: "Social Media App",
     img: "./assets/bank-itau.png",
-  },
-  {
-    id: 5,
-    title: "Social Media App",
-    img: "./assets/bank-itau.png",
+    url: "https://google.com"
   },
 ];
 
@@ -87,26 +84,25 @@ export const designPortfolio = [
     id: 1,
     title: "Social Media App",
     img: "./assets/meeting.png",
+    url: "https://google.com"
   },
   {
     id: 2,
     title: "Social Media App",
     img: "./assets/bank-itau.png",
+    url: "https://google.com"
   },
   {
     id: 3,
     title: "Social Media App",
     img: "./assets/bank-itau.png",
+    url: "https://google.com"
   },
   {
     id: 4,
     title: "Social Media App",
     img: "./assets/bank-itau.png",
-  },
-  {
-    id: 5,
-    title: "Social Media App",
-    img: "./assets/bank-itau.png",
+    url: "https://google.com"
   },
 ];
 
@@ -115,25 +111,24 @@ export const contentPortfolio = [
     id: 1,
     title: "Social Media App",
     img: "./assets/earth.png",
+    url: "https://google.com"
   },
   {
     id: 2,
     title: "Social Media App",
     img: "./assets/bank-itau.png",
+    url: "https://google.com"
   },
   {
     id: 3,
     title: "Social Media App",
     img: "./assets/bank-itau.png",
+    url: "https://google.com"
   },
   {
     id: 4,
     title: "Social Media App",
     img: "./assets/bank-itau.png",
-  },
-  {
-    id: 5,
-    title: "Social Media App",
-    img: "./assets/bank-itau.png",
+    url: "https://google.com"
   },
 ];
